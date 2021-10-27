5e4e62f11b73d89d504c45925d753bfb5b5639e5
pedronieto84 <pedro.nieto.sanchez@gmail.com>
2021-10-27 16:52:38 +0200

    Get Courses, modules y module funcionan

--- src/index.ts
+++ src/index.ts
@@ -2,60 +2,92 @@ import * as functions from "firebase-functions";
 import * as admin from 'firebase-admin';
 admin.initializeApp(functions.config().firebase)
 const db = admin.firestore();
+const cors = require('cors')({origin: true});
 
 
- export const getModule = functions.https.onRequest((request, response) => {
-   functions.logger.info("Hello logs!", {structuredData: true});
-   response.send("Hello");
+const getCollectionData = async (whatCollection:string, filterName?:any, filterField?:any) => {
+      let  modulesArray; 
+            if(filterName){
+                 modulesArray = await db.collection(whatCollection).where(filterName, '==', parseInt(filterField)).get()
+                 
+                }else{
+                 modulesArray = await db.collection(whatCollection).get()
+                }
+            const final =  modulesArray.docs.map((x)=>{
+                return x.data()
+            })
+            return final;
+}
 
- });
-
-  export const getModules = functions.https.onRequest((request, response) => {
-   functions.logger.info("Hello logs!", {structuredData: true});
-   response.send("Hello");
- }); 
- 
-
- 
- export const getCourses =  functions.https.onRequest( async (request, response) => {
-
-    try{
-        const courses = await db.collection('courses').get();  
-        const finalResponse = courses.docs.map((eachDoc) => {
-            return eachDoc.data()
-        })
-        console.log('courses', courses)
-        response.send(finalResponse)
-    }catch(e){
-        console.log('error', e)
+    const getQueryData = (request: functions.https.Request) => {
+                const keyName = Object.keys(request.query).length > 0 ? Object.keys(request.query)[0] : false
+                const filterToQuery = request.query.courseId;
+                return { keyName , filterToQuery }
     }
 
+ export const getModule = functions.https.onRequest(async  (request, response) => {
+        response.set('Access-Control-Allow-Origin', '*');
 
- }); 
+        //const moduleId = request.params
+        console.log('params', request.params['0'])
+        console.log('path', request.path)
+        const moduleId = request.params['0'];
+        /*
+        let module = {
+            moduleId: moduleId,
+            topics: [],
+            title: '',
+            description: '',
+            imageUrl: '',
+            route: '',
+            courseId: 1
+        } */
+
+        const doc = await db.collection('modules').where('moduleId', '==', parseInt(moduleId) ).get();
+        const document = doc.docs[0].data();
+        
+
+        response.send(document)
+      //  const data = await getCollectionData(request,  'modules', 'moduleId')
+      //  response.send(data);
+ });
+
+  export const getModules = functions.https.onRequest(async (request, response) => {
+       response.set('Access-Control-Allow-Origin', '*');
+        cors(request, response, async ()=>{
+            let  modulesArray; 
+            if(request.query.courseId){
+                const courseId = parseInt(request.query.courseId as string)
+                 modulesArray = await db.collection('modules').where('courseId', '==', courseId).get()
+            }else{
+                 modulesArray = await db.collection('modules').get()
+            }
+            const cleanedData = modulesArray.docs.map((x)=>{
+                return x.data()
+            })
+           return response.status(200).send(cleanedData) 
+ });
+ })
+
+ export const getCourses =  functions.https.onRequest( async (request, response) => {
+       response.set('Access-Control-Allow-Origin', '*');
+        cors(request, response, async ()=>{
+            let cleanedData = await getCollectionData('courses', getQueryData(request).keyName , getQueryData(request).filterToQuery)
+            
+            return response.status(200).send(cleanedData) 
+        });
+ });  
  
  export const getCourse = functions.https.onRequest( async  (request, response) => {
-
-    const courseId = request.body.courseId;
-    console.log('courseId', courseId);
-    
-    
-    try{
-        const course = await db.collection('courses').doc(courseId.toString()).get()
-        const data = course.data()
-        console.log(data);
-        
-        response.send(data);
-    }catch(e){
-        console.log(e);
-        
-    }
-    
+        response.set('Access-Control-Allow-Origin', '*');
+     //   const data = await getCollectionData(request,  'courses', 'courseId')
+     //   response.send(data);
  });
  
- 
- export const getTopics = functions.https.onRequest((request, response) => {
-   functions.logger.info("Hello logs!", {structuredData: true});
-   response.send("Hello");
+ export const getTopics = functions.https.onRequest(async (request, response) => {
+       response.set('Access-Control-Allow-Origin', '*');
+    //   const data = await getCollectionData(request, 'topics')
+    //   response.send(data);
  }); 
 
 
